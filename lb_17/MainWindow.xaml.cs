2aa19efb29c0c6d4fb7c6e37b587e5fe660a7ec9
pr111aksenov <pr111aksenov@1-510-CLS-07>
2023-12-18 14:18:22 +0600

    Добавьте файлы проекта.

---
+++ lb_17/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace lb_17
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        PersonModel Tom;
+        public MainWindow()
+        {
+            InitializeComponent();
+            Tom = new PersonModel();
+            this.DataContext = Tom;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show($"Name: {Tom.Name}\nAge: {Tom.Age}\nPosition: {Tom.Position}");
+        }
+    }
+}
---
+++ lb_17/PersonModel.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace lb_17
+{
+    public class PersonModel : IDataErrorInfo
+    {
+        public string Name { get; set; } = String.Empty;
+        public int Age { get; set; }
+        public string Position { get; set; }
+        public string this[string columnName]
+        {
+            get
+            {   
+                string error = String.Empty;
+                switch (columnName)
+                {
+                    case "Age":
+                        if (Age < 0 || Age > 100)
+                        {
+                            error = "Возраст должен быть больше 0 и меньше 100";
+                        }
+                        break;
+                    case "Name":
+                        if (Name.Equals(String.Empty) || !Regex.IsMatch(Name, @"^[а-яА-Я ]+$") || Regex.IsMatch(Name.Trim(), @"[ \t]{2,}"))
+                        {
+                            error = "Имя должно содержать только русский язык и пробелы";
+                        }
+                        break;
+                    case "Position":
+                        break;
+                }
+                return error;
+            }
+        }
+        public string Error
+        {
+            get { throw new NotImplementedException(); }
+        }
+    }
+}
